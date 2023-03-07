f4f53358d302f7853259f44a69faef83eb3b8c3f
Ivan Ivanov <ivan.ivanov@endava.com>
2023-03-07 17:17:48 +0200

    add Solution

--- task1/Main.java
+++ task1/Main.java
@@ -1,12 +1,1 @@
-import java.util.*;
-
-public class Main {
-
-      public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-
-        Solution solution = new Solution();
-        System.out.println(solution.isPalindrome(number));
-    }
-}
+import java.util.*; public class Main {public static void main(String[] args) {Scanner scanner = new Scanner(System.in);int number = scanner.nextInt();Solution solution = new Solution();System.out.println(solution.isPalindrome(number));}}
